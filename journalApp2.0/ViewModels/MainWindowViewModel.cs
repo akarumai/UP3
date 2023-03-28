ccfd7706ecfe3953e15b95d3434845d6767dabfb
llDimAssll <101548861+llDimAssll@users.noreply.github.com>
2023-03-28 15:40:25 +0300

    Добавьте файлы проекта.

---
+++ journalApp2.0/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using journalApp2._0.ViewModels.Base;
+
+namespace journalApp2._0.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string title="УП наше крутое";
+        /// <summary>
+        /// Заголовок окна 
+        /// </summary>
+        public string Title
+        {
+            get { return title; }
+            set { Set(ref title, value); }
+        }
+    }
+}
